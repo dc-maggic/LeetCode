c1ab4c6b813344ae7396ad994782967444e20a02
maggic <dc-maggic@outlook.com>
2021-07-06 17:31:41 +0800

    中序遍历，总是选择中间位置左边的数字作为根节点

---
+++ 108.将有序数组转换为二叉搜索树.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=javascript
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+ var sortedArrayToBST = function(nums) {
+    return buildBST(nums, 0, nums.length-1)
+    function buildBST(nums, left, right){
+        if(left > right){
+            return null
+        }
+        const mid= Math.floor((left+right)/2), node= new TreeNode(nums[mid])
+        node.left= buildBST(nums, left, mid-1)
+        node.right= buildBST(nums, mid+1, right)
+        return node
+    }
+};
+// @lc code=end
+
