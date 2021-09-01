503d959017376838507334f006aa4a6b14763f82
maggic <dc-maggic@outlook.com>
2021-09-01 11:18:27 +0800

    矩阵快速幂

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -11,14 +11,29 @@
  */
 var fib = function(n) {
     if(n<2) return n
-    let ary = [0,1,1], i = 2
-    while(i<=n) {
-        ary[2] = ary[1] + ary[0]
-        ary[0] = ary[1]
-        ary[1] = ary[2]
-        i++
-    }
-    return ary[2]
+    const m = [[1,1], [1,0]],
+        res = pow(m, n-1)
+    return res[0][0]
 };
+function pow(m , n) {
+    let ret = [[1,0], [0,1]]
+    while(n>0) {
+        if((n&1)===1) {
+            ret = multiply(ret, m)
+        }
+        n>>=1
+        m = multiply(m,m)
+    }
+    return ret;
+}
+function multiply(a, b) {
+    const c = new Array(2).fill(0).map(() => new Array(2).fill(0));
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < 2; j++) {
+            c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+        }
+    }
+    return c;
+}
 // @lc code=end
 
